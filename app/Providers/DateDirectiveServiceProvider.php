426835cb1794a8ad9194780d90d20bcdae606438
Aron Duby <aron.duby@gmail.com>
2026-08-08 18:57:21 -0400

    update date directive to make diffInDates use absolute value

--- app/Providers/DateDirectiveServiceProvider.php
+++ app/Providers/DateDirectiveServiceProvider.php
@@ -76,7 +76,7 @@ class DateDirectiveServiceProvider extends ServiceProvider
         if ($d->isToday()) {
             return trans('misc.today');
 
-        } elseif ($d->diffInDays() < 6) {
+        } elseif ($d->diffInDays(absolute: true) < 6) {
             return $d->format($full ? self::DAY : self::DAY_SHORT);
 
         } else {
